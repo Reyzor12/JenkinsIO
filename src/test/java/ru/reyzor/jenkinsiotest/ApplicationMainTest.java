a606505babb18124bf23c17b13ca8e81abfe5a3e
reyzor <testaccwow001@gmail.com>
2018-08-22 15:56:09 +0300

    123

---
+++ src/test/java/ru/reyzor/jenkinsiotest/ApplicationMainTest.java
@@ -0,0 +1,11 @@
+package ru.reyzor.jenkinsiotest;
+
+import org.junit.Test;
+
+public class ApplicationMainTest {
+   @Test
+    public void createDeployment() {
+       System.out.println("Hello world");
+    }
+
+}
